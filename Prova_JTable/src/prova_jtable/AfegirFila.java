dfd8fe80bea81205b417913f5986572f2de1ea63
1dam <1dam@WINMAC2.alumnado.flo>
2016-04-18 17:35:21 +0200

    heuya gfhd

--- Prova_JTable/src/prova_jtable/AfegirFila.java
+++ Prova_JTable/src/prova_jtable/AfegirFila.java
@@ -12,6 +12,9 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JTextField;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -20,7 +23,9 @@ public class AfegirFila extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 	DefaultTableModel dtm2;
+	Object[] columnes;
 	Object[] dades;
+	JTextField[] arrayJT;
 	/**
 	 * Launch the application.
 	 */
@@ -31,24 +36,39 @@ public class AfegirFila extends JDialog {
 	 */
 	public AfegirFila(DefaultTableModel dtm,Object[] valor) {
 		dtm2=dtm;
-		dades=valor;
+		columnes=valor;
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(new GridLayout(0, 1, 0, 0));
 		{
-			JScrollPane scrollPane = new JScrollPane();
-			contentPanel.add(scrollPane);
+			
+			
 			{
 				JPanel panelDades = new JPanel();
+				panelDades.setLayout(new GridLayout(0, 2, columnes.length, 0));
 				{
-					for(int i=0;i<dades.length;i++){
+					arrayJT=new JTextField[columnes.length];
+					dades=new Object[columnes.length];
+					for(int i=0;i<columnes.length;i++){
 						JLabel label =new JLabel();
+						{
+							label.setText(dtm2.getColumnName(i)+" : ");
+						}
+						panelDades.add(label);
+						arrayJT[i]=new JTextField();
+						{
+							
+						}
+						panelDades.add(arrayJT[i]);
 					}
+					
 				}
+				JScrollPane scrollPane = new JScrollPane(panelDades);
 				
-				scrollPane.setViewportView(panelDades);
+				
+				contentPanel.add(scrollPane);
 			}
 		}
 		{
@@ -60,6 +80,17 @@ public class AfegirFila extends JDialog {
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
+				okButton.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						for(int i=0;i<columnes.length;i++){
+							dades[i]=arrayJT[i].getText();
+						}
+						dtm2.addRow(dades);
+					}
+				});
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
